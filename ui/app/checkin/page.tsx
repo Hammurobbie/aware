f7d5391fd5bba6243938352ef06f92d2238695e1
hammurobbie <rthomporter@gmail.com>
2024-02-25 01:26:28 -0600

    keys

--- ui/app/checkin/page.tsx
+++ ui/app/checkin/page.tsx
@@ -19,11 +19,16 @@ export default async function Checkin() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="max-w-5xl w-full flex flex-wrap">
-        {data?.map((wbc: any) => (
-          <div className="flex flex-col justify-center items-center p-5 m-5 bg-slate-800 rounded-xl">
+        {data?.map((wbc: any, key: number) => (
+          <div
+            key={key}
+            className="flex flex-col justify-center items-center p-5 m-5 bg-slate-800 rounded-xl"
+          >
             <div>{wbc?.notes}</div>
-            {wbc?.updated_emotions?.map((e: any) => (
-              <p className="text-slate-400 pt-2">{e.name}</p>
+            {wbc?.updated_emotions?.map((e: any, key: number) => (
+              <p key={key} className="text-slate-400 pt-2">
+                {e.name}
+              </p>
             ))}
           </div>
         ))}
