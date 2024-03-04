e9b5c9c10698f0aa7c6cbc1d7cc5fa02e816f93b
hammurobbie <rthomporter@gmail.com>
2024-03-03 19:33:51 -0600

    cur nav cue

--- ui/app/components/FooterNav.tsx
+++ ui/app/components/FooterNav.tsx
@@ -1,16 +1,35 @@
+"use client";
 import Image from "next/image";
 import logo from "../../public/logo.png";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const FooterNav = () => {
+  const curPage = usePathname()?.substring(1);
+  const pages = [
+    "monitor",
+    <Image alt="logo" src={logo} width={100} />,
+    "settings",
+  ];
   return (
     <header className="sticky bottom-0 z-10 flex justify-center items-center w-full h-16 bg-bg-secondary text-grayscale border-t-4 border-error">
       <div className="flex justify-around items-center w-full max-w-2xl mx-auto">
-        <Link href="/monitor">monitor</Link>
-        <Link href="/">
-          <Image alt="logo" src={logo} width={100} />
-        </Link>
-        <Link href="/settings">settings</Link>
+        {pages.map((p, i) => {
+          const isLogo = typeof p !== "string";
+          const isCurPage = isLogo && !curPage ? true : curPage === p;
+          return (
+            <Link
+              key={i}
+              href={isLogo ? "/" : p}
+              className={isCurPage ? "animate-pulse" : ""}
+            >
+              {!isLogo ? (
+                <span className={isCurPage ? "text-error" : ""}>_</span>
+              ) : null}
+              {p}
+            </Link>
+          );
+        })}
       </div>
     </header>
   );
